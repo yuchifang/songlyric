bc3aa3f45817229cf5e9452c26a37a38954cee68
YuChiFang <fangfelipe@gmail.com>
2018-10-11 17:05:10 +0800

    Updates

---
+++ precache-manifest.2902f0aa71c56064432c3a20ff52c930.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdf07371060181a54eee",
+    "url": "/songlyric/static/js/runtime~main.fdf07371.js"
+  },
+  {
+    "revision": "b9925c16b20164b9314e",
+    "url": "/songlyric/static/js/main.b9925c16.chunk.js"
+  },
+  {
+    "revision": "602b3d62118f1a372dbc",
+    "url": "/songlyric/static/js/1.602b3d62.chunk.js"
+  },
+  {
+    "revision": "b9925c16b20164b9314e",
+    "url": "/songlyric/static/css/main.03934502.chunk.css"
+  },
+  {
+    "revision": "005cf4e937bdc0028a9d2f9e507210de",
+    "url": "/songlyric/index.html"
+  }
+];
